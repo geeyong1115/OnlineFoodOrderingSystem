11765a5742ed26280461a4dbb884b299a8900ecc
Tay Gee Yong <53674303+geeyong1115@users.noreply.github.com>
2022-04-07 21:20:16 +0800

    Update OrderDetails.php

--- app/Models/OrderDetails.php
+++ app/Models/OrderDetails.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderDetails extends Model
 {
+    //
     use HasFactory;
+    protected $table = 'order_details';
+    protected $fillable = [
+        'order_id',
+        'food_id',
+        'price',
+        'quantity',
+
+    ];
+
+    public function orderdetails(){
+        return $this->hasMany(OrderDetails::class,'order_id');
+    }
+
 }
